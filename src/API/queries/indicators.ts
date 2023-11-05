8cb29bc89cb1c3025ee6ee7b407f5d52a98a5480
Bassem Ghazouani <bassem.ghazouani@hotmail.com>
2023-11-05 22:24:00 +0100

    refactor: update the table rows and add id to be consumed when calling graphql fetch indicator

--- src/API/queries/indicators.ts
+++ src/API/queries/indicators.ts
@@ -3,6 +3,18 @@ import { gql } from "@apollo/client";
 export const GET_INDICATORS = gql`
   query GetIndicators {
     indicators {
+      id
+      variable
+      timestamp
+      turbineId
+    }
+  }
+`;
+
+export const GET_INDICATORS_BY_ID = gql`
+  query GetIndicatorsById($id: Int!) {
+    indicator(id: $id) {
+      id
       variable
       timestamp
       turbineId
--- src/components/indicatorTable/constants.ts
+++ src/components/indicatorTable/constants.ts
@@ -1,4 +1,5 @@
 export const TABLE_HEADER_LABELS: string[] = [
+  "Id",
   "Turbine id",
   "Variable",
   "Timestamp",
