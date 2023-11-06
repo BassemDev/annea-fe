83cb0718021b2be28bcda6341825b243f59a3e40
Bassem Ghazouani <bassem.ghazouani@hotmail.com>
2023-11-06 03:14:49 +0100

    feat: revert changes related to the config attributes

--- src/config/environmentVariableConfig.ts
+++ src/config/environmentVariableConfig.ts
@@ -2,7 +2,8 @@ import { EnvironmentConfig } from "./types";
 
 export const environmentVariableConfig: EnvironmentConfig = {
   graphqlAPI:
-    process.env.REACT_APP_GRAPHQL_API || "http://localhost:3001/graphql",
+    process.env.REACT_APP_GRAPHQL_API ||
+    "https://annea-backend-service.onrender.com/graphql",
   apiToken:
     process.env.REACT_APP_AUTHORIZATION_API_TOKEN ||
     "ApiKeyca03na188ame03u1d78620de67282882a84",
