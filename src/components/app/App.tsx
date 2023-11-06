7228b01ca6b33a59b14f29a8723273948dc85fb4
Bassem Ghazouani <bassem.ghazouani@hotmail.com>
2023-11-06 06:54:16 +0100

    chore: update the app component adapt it to the new change of routing

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 
 import { IndicatorView } from "../indicatorView/IndicatorView";
+import styled from "styled-components";
 
-export const App = () => {
+const EmptyContainer = styled.div`
+  margin-top: 150px;
+`;
+export const App: React.FunctionComponent = () => {
   return (
     <>
+      <EmptyContainer />
       <IndicatorView />
     </>
   );
